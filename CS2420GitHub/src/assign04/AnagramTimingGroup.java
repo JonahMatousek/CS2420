453b76232a8ed55586fbe0001a3e74db49f05fee
cp <cp@caseys-air-2.uconnect.utah.edu>
2022-09-22 17:58:37 -0600

    test

--- CS2420GitHub/src/assign04/AnagramTimingGroup.java
+++ CS2420GitHub/src/assign04/AnagramTimingGroup.java
@@ -5,14 +5,14 @@ import java.util.Random;
 
 
 
-public class AnagramCheckerTiming {
+public class AnagramTimingGroup {
 	private static ArrayList<String> generateStringArray(int size){
-		String[] alpha = new String[] {"a","b","c","d","e","f","g","h","i","j","k","l","m"};
+		String[] alpha = new String[] {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
 		ArrayList<String> stringArrayRand = new ArrayList<>();
 		while(stringArrayRand.size()< size) {
 			String newString = "";
-			while(newString.length()< size/100) {
-				newString+=(alpha[rand.nextInt(13)]);
+			while(newString.length()< 10) {
+				newString+=(alpha[rand.nextInt(26)]);
 				
 			}
 			stringArrayRand.add(newString);
@@ -27,13 +27,14 @@ public class AnagramCheckerTiming {
 		rand.setSeed(System.currentTimeMillis());
 	
 		//TODO: Write code to time your toSortedList, contains, and SearchUtil.binarySearch methods so you can plot the results.
-		int timesToLoop = 10000;
+		int timesToLoop = 5000;
 		
 		for (int n = 1000; n <= 20000; n += 1000) {
 			// Generate a new "random" ArrayCollection of size n.
 			String[] StringArray = generateStringArray(n).toArray(new String[0]);
-			String val1 = StringArray[rand.nextInt(n)];
-			String val2 = StringArray[rand.nextInt(n)];
+			
+			//String val1 = StringArray[rand.nextInt(n)];
+			//String val2 = StringArray[rand.nextInt(n)];
 			long startTime, midpointTime, stopTime;
 			// First, spin computing stuff until one second has gone by.
 			// This allows this thread to stabilize.
@@ -43,7 +44,7 @@ public class AnagramCheckerTiming {
 			// Now, run the test.
 			startTime = System.nanoTime();
 			for (int i = 0; i < timesToLoop; i++) {
-				AnagramChecker.areAnagrams(val1,val2);
+				AnagramChecker.getLargestAnagramGroup(StringArray);
 			}
 			midpointTime = System.nanoTime();
 			// Run a loop to capture the cost of running the "timesToLoop" loop 
@@ -70,3 +71,5 @@ public class AnagramCheckerTiming {
 		return rand.nextInt();
 	}
 }
+
+
