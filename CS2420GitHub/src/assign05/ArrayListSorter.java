349ad89b35ec2a77c318d0d453d4970010ce2eaf
cp <cp@caseys-air-2.uconnect.utah.edu>
2022-10-04 16:15:05 -0600

    Everything complete except sort methods

--- CS2420GitHub/src/assign05/ArrayListSorter.java
+++ CS2420GitHub/src/assign05/ArrayListSorter.java
@@ -1,6 +1,7 @@
 package assign05;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ArrayListSorter {
 
@@ -18,32 +19,53 @@ public class ArrayListSorter {
 	 */
 	public static <T extends Comparable<? super T>> void quicksort(ArrayList<T> list) {
 		
+		
+		
 	}
 	
 	/**
-	 * @param size
-	 * @return
+	 * @param size list length
+	 * @return return acending int list
 	 */
 	public static ArrayList<Integer> generateAscending(int size){
-		return new ArrayList<Integer>();
+		ArrayList<Integer> out  = new ArrayList<>();
+		for(int i = 0; i < size;i++) {
+			out.add(i);
+			
+		}
+		
+		return out;
 		
 	}
 	
 	/**
-	 * @param size
-	 * @return
+	 * @param size list length
+	 * @return returns permutted int list
 	 */
 	public static ArrayList<Integer> generatePermuted(int size){
-		return new ArrayList<Integer>();
+		ArrayList<Integer> out  = new ArrayList<>();
+		for(int i = 0; i < size;i++) {
+			out.add(i);
+			
+		}
+		Collections.shuffle(out);
+		
+		return out;
 		
 	}
 	
 	/**
-	 * @param size
-	 * @return
+	 * @param size list length
+	 * @return decending int list
 	 */
 	public static ArrayList<Integer> generateDescending(int size){
-		return new ArrayList<Integer>();
+		ArrayList<Integer> out  = new ArrayList<>();
+		for(int i = size; i > 0;i--) {
+			out.add(i);
+			
+		}
+		
+		return out;
 	}
 	
 	
