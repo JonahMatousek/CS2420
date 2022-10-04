e754249b0465295b4a27a856259424e58e34e118
cp <cp@caseys-air-2.uconnect.utah.edu>
2022-10-04 15:52:32 -0600

    new package and file

---
+++ CS2420GitHub/src/assign05/ArrayListSorter.java
@@ -0,0 +1,50 @@
+package assign05;
+
+import java.util.ArrayList;
+
+public class ArrayListSorter {
+
+	/**
+	 * @param <T>
+	 * @param list
+	 */
+	public static <T extends Comparable<? super T>> void mergesort(ArrayList<T> list) {
+		
+	}
+	
+	/**
+	 * @param <T>
+	 * @param list
+	 */
+	public static <T extends Comparable<? super T>> void quicksort(ArrayList<T> list) {
+		
+	}
+	
+	/**
+	 * @param size
+	 * @return
+	 */
+	public static ArrayList<Integer> generateAscending(int size){
+		return new ArrayList<Integer>();
+		
+	}
+	
+	/**
+	 * @param size
+	 * @return
+	 */
+	public static ArrayList<Integer> generatePermuted(int size){
+		return new ArrayList<Integer>();
+		
+	}
+	
+	/**
+	 * @param size
+	 * @return
+	 */
+	public static ArrayList<Integer> generateDescending(int size){
+		return new ArrayList<Integer>();
+	}
+	
+	
+}
