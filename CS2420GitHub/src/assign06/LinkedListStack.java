1e6f38cedc5bba1c77859ab8a422525ec76f881e
jonah <jonah@10.0.0.145>
2022-10-19 17:46:44 -0600

    Created LinkedListStack.java

---
+++ CS2420GitHub/src/assign06/LinkedListStack.java
@@ -0,0 +1,43 @@
+package assign06;
+
+import java.util.NoSuchElementException;
+
+public class LinkedListStack<T> implements Stack<T>{
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T peek() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T pop() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void push(Object element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
