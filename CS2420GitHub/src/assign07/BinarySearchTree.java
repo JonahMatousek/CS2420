168154955e70cb5f6942d67561453c4306154558
cp <cp@caseys-air-2.uconnect.utah.edu>
2022-10-26 18:01:06 -0600

    dont know why commit didnt work

--- CS2420GitHub/src/assign07/BinarySearchTree.java
+++ CS2420GitHub/src/assign07/BinarySearchTree.java
@@ -11,68 +11,107 @@ import java.util.NoSuchElementException;
  */
 public class BinarySearchTree<T extends Comparable<? super T>> implements SortedSet<T> {
 
-
-private TreeNode root;
-private int height;
-private int size;
-	
-	BinarySearchTree(){
-		
-		root = null;
-		height = 0;
-		size = 0;
-	}
-	
 	class TreeNode {
 		private T val;
 		private TreeNode leftChild;
 		private TreeNode rightChild;
-		private BinarySearchTree subTree;
-		private int depth;
 		
-		TreeNode(T val){
+
+		TreeNode(T val) {
 			this.val = val;
 			leftChild = null;
 			rightChild = null;
-			subTree = null;
-			depth = 0;
+
 		}
+
 	}
+
+	private TreeNode root;
+	private ArrayList<T> listTree;
+
+	private int size;
+
+	BinarySearchTree() {
+
+		root = null;
+		size = 0;
+		listTree = new ArrayList<>();
+	}
+
 	@Override
 	public boolean add(T item) {
-		if(root == null)
-			root.val = item;
+		//if (this.contains(item))
+			//return false;
+		root = add(root, item);
+		size++;
 		return true;
+	}
+
+	private TreeNode add(TreeNode root, T item) {
+		if (root == null)
+			return new TreeNode(item);
+
+		else if (item.compareTo(root.val) < 0)
+			root.leftChild = add(root.leftChild, item);
+		else if (item.compareTo(root.val) > 0)
+			root.rightChild = add(root.rightChild, item);
+
+		return root;
 	}
 
 	@Override
 	public boolean addAll(Collection<? extends T> items) {
-		// TODO Auto-generated method stub
-		return false;
+		for (T item : items) {
+			add(item);
+		}
+		return true;
 	}
 
 	@Override
 	public void clear() {
 		root = null;
-		
 	}
 
 	@Override
 	public boolean contains(T item) {
-		// TODO Auto-generated method stub
-		return false;
+		if(this.root.val.compareTo(item) == 0)
+			return true;
+		return contains(root,item);
+		
+	}
+	
+	private boolean contains(TreeNode root, T item) {
+		if (root == null)
+			return false;
+		else if(root.val.compareTo(item)==0)
+			return true;
+		else if (root.val.compareTo(item) > 0)
+			return contains(root.leftChild,item);
+		else
+			return contains(root.rightChild,item);
 	}
 
 	@Override
 	public boolean containsAll(Collection<? extends T> items) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		for(T item:items) {
+			if(this.contains(item)==false)
+				return false;
+	
+		}
+		return true;
 	}
 
 	@Override
 	public T first() throws NoSuchElementException {
-		if(root == null)
+		if (root == null)
 			throw new NoSuchElementException();
+		else
+			return first(root);
+	}
+	private T first(TreeNode root) {
+		if(root.leftChild != null)
+			return first(root.leftChild);
 		else
 			return root.val;
 	}
@@ -87,34 +126,90 @@ private int size;
 
 	@Override
 	public T last() throws NoSuchElementException {
-		
-		return null;
+		if (root == null)
+			throw new NoSuchElementException();
+		else
+			return last(root);
+	}
+
+	public T last(TreeNode root) {
+		if (root.rightChild != null)
+			return last(root.rightChild);
+		else
+			return root.val;
+
 	}
 
 	@Override
 	public boolean remove(T item) {
-		// TODO Auto-generated method stub
-		return false;
+		TreeNode out = remove(root,item);
+		size--;
+		if(out==null)
+			return false;
+		else
+			return true;
+		
 	}
-
+	
+	private TreeNode remove(TreeNode root,T item) {
+		if(root == null)
+			return root;
+		
+		else if(root.val.compareTo(item) > 0) {
+			root.leftChild = remove(root.leftChild,item);
+		}else if(root.val.compareTo(item) < 0)
+			root.rightChild = remove(root.rightChild,item);
+		else {
+			if(root.leftChild == null)
+				return root.rightChild;
+			else if(root.rightChild == null)
+				return root.leftChild;
+			root.val = first(root.rightChild);
+		
+		
+			root.rightChild = remove(root.rightChild,root.val);
+		}
+		return root;
+			
+		
+	}
+	
 	@Override
 	public boolean removeAll(Collection<? extends T> items) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		for(T item:items) {
+			if (remove(item) == false)
+				return false;
+		}
+		return true;
 	}
 
 	@Override
 	public int size() {
-		
 		return size;
 	}
 
 	@Override
 	public ArrayList<T> toArrayList() {
-		// TODO Auto-generated method stub
-		return null;
+		listTree = new ArrayList<>();
+		return toArrayList(root);
 	}
 
-	
-	
+	public ArrayList<T> toArrayList(TreeNode head) {
+		//ArrayList<T> sortedTreeList = new ArrayList<T>();
+		
+		if (head != null) {
+			toArrayList(head.leftChild);
+			//System.out.println(head.val);
+			listTree.add(head.val);
+			toArrayList(head.rightChild);
+		}
+			
+		
+			
+			return listTree;
+		
+		
+	}
+
 }
---
+++ CS2420GitHub/src/assign07/BinarySearchTreeTest.java
@@ -0,0 +1,218 @@
+package assign07;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+import org.junit.jupiter.api.Test;
+
+
+
+class BinarySearchTreeTest {
+
+	@Test
+	void testAddSimple() {
+		BinarySearchTree<String> tree = new BinarySearchTree<>();
+		tree.add("Hello");
+		ArrayList<String> correct = new ArrayList<>();
+		correct.add("Hello");
+
+		//System.out.println(tree.toArrayList().toString());
+		assertEquals(correct.toString(), tree.toArrayList().toString());
+	}
+	
+	@Test
+	void testAdd() {
+		BinarySearchTree<String> tree = new BinarySearchTree<>();
+		tree.add("Hello");
+		tree.add("Bye");
+		tree.add("Apple");
+		ArrayList<String> correct = new ArrayList<>();
+		correct.add("Apple");
+		correct.add("Bye");
+		correct.add("Hello");
+
+		assertEquals(correct.toString(), tree.toArrayList().toString());
+	}
+
+	@Test
+	void testAddAll() {
+		BinarySearchTree<String> tree = new BinarySearchTree<>();
+		Collection<String> list = new ArrayList<String>();
+		list.add("No");
+		list.add("Hello");
+		list.add("Bye");
+
+		tree.addAll(list);
+
+		ArrayList<String> correct = new ArrayList<>();
+		correct.add("Bye");
+		correct.add("Hello");
+		correct.add("No");
+
+		//System.out.println(tree.toArrayList().toString());
+		assertEquals(correct.toString(), tree.toArrayList().toString());
+	}
+	
+	@Test
+	void testClear() {
+		BinarySearchTree<String> tree = new BinarySearchTree<>();
+		tree.add("Hello");
+		tree.add("Bye");
+		tree.add("Apple");
+		tree.clear();
+		ArrayList<String> correct = new ArrayList<>();
+		assertEquals(correct.toString(),tree.toArrayList().toString());
+	}
+	
+	@Test
+	void testContains() {
+		BinarySearchTree<String> tree = new BinarySearchTree<>();
+		tree.add("Hello");
+		tree.add("Bye");
+		tree.add("Apple");
+		tree.add("Yams");
+		tree.add("Crabs");
+		tree.add("Train");
+		assertTrue(tree.contains("Yams"));
+	}
+	
+	@Test
+	void testContainsAll() {
+		BinarySearchTree<String> tree = new BinarySearchTree<>();
+		tree.add("Hello");
+		tree.add("Bye");
+		tree.add("Apple");
+		tree.add("Yams");
+		tree.add("Crabs");
+		tree.add("Train");
+		tree.add("No");
+		Collection<String> list = new ArrayList<String>();
+		list.add("No");
+		list.add("Hello");
+		list.add("Bye");
+		assertTrue(tree.containsAll(list));
+	}
+	
+	@Test
+	void testFirst() {
+		BinarySearchTree<String> tree = new BinarySearchTree<>();
+		tree.add("Hello");
+		tree.add("Bye");
+		tree.add("Apple");
+		tree.add("Yams");
+		tree.add("Crabs");
+		tree.add("Train");
+		tree.add("No");
+		assertEquals("Apple",tree.first());
+	}
+	@Test
+	void testLast() {
+		BinarySearchTree<String> tree = new BinarySearchTree<>();
+		tree.add("Hello");
+		tree.add("Bye");
+		tree.add("Apple");
+		tree.add("Yams");
+		tree.add("Crabs");
+		tree.add("Train");
+		tree.add("No");
+		assertEquals("Yams",tree.last());
+	}
+	
+	@Test 
+	void testIsEmpty() {
+		BinarySearchTree<String> tree = new BinarySearchTree<>();
+		assertTrue(tree.isEmpty());
+	}
+	@Test 
+	void testIsEmptyFalse() {
+		BinarySearchTree<String> tree = new BinarySearchTree<>();
+		tree.add("Hello");
+		assertFalse(tree.isEmpty());
+	}
+	@Test 
+	void testRemove() {
+		BinarySearchTree<String> tree = new BinarySearchTree<>();
+		tree.add("Hello");
+		tree.add("Bye");
+		tree.add("Apple");
+		tree.add("Yams");
+		tree.add("Crabs");
+		tree.add("Train");
+		tree.add("No");
+		tree.remove("No");
+		ArrayList<String> correct= new ArrayList<>();
+		correct.add("Apple");
+		correct.add("Bye");
+		correct.add("Crabs");
+		correct.add("Hello");
+		correct.add("Train");
+		correct.add("Yams");
+		
+		
+		assertEquals(correct.toString(),tree.toArrayList().toString());
+	}
+	
+	@Test
+	void testRemoveAll() {
+		BinarySearchTree<String> tree = new BinarySearchTree<>();
+		tree.add("Hello");
+		tree.add("Bye");
+		tree.add("Apple");
+		tree.add("Yams");
+		tree.add("Crabs");
+		tree.add("Train");
+		tree.add("No");
+		Collection<String> list = new ArrayList<String>();
+		list.add("Train");
+		list.add("Apple");
+		list.add("Hello");
+		list.add("Yams");
+		tree.removeAll(list);
+		ArrayList<String> correct= new ArrayList<>();
+		correct.add("Bye");
+		correct.add("Crabs");
+		correct.add("No");
+		assertEquals(correct.toString(),tree.toArrayList().toString());
+		
+	}
+	@Test
+	void testSize() {
+		BinarySearchTree<String> tree = new BinarySearchTree<>();
+		tree.add("Hello");
+		tree.add("Bye");
+		tree.add("Apple");
+		tree.add("Yams");
+		tree.add("Crabs");
+		tree.add("Train");
+		tree.add("No");
+		
+		
+		
+		assertEquals(7,tree.size());
+	}
+	
+	@Test
+	void testToArrayList() {
+		BinarySearchTree<String> tree = new BinarySearchTree<>();
+		tree.add("Hello");
+		tree.add("Bye");
+		tree.add("Apple");
+		tree.add("Yams");
+		tree.add("Crabs");
+		tree.add("Train");
+		
+		ArrayList<String> correct= new ArrayList<>();
+		correct.add("Apple");
+		correct.add("Bye");
+		correct.add("Crabs");
+		correct.add("Hello");
+		correct.add("Train");
+		correct.add("Yams");
+		assertEquals(correct.toString(),tree.toArrayList().toString());
+		
+	}
+
+}
